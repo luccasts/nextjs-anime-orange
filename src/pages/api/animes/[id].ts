b533a27c40430632f693b51d3fd0751da58d59c6
Lucasts <lucastsilvadev@gmail.com>
2025-04-24 01:24:27 -0300

    resolving IDAnimes and animes type error

--- src/pages/api/animes/[id].ts
+++ src/pages/api/animes/[id].ts
@@ -1,41 +1,37 @@
 import { IAnimeAPI, IAnimeCarouselAPI, ResponseError } from "interfaces";
 import { NextApiRequest, NextApiResponse } from "next";
 import { Api } from "../../../../data";
-interface animeI {
-  anime:
-    | {
-        id: string;
-        title: string;
-        eps: {
-          id: string;
-          ep: string;
-          duration: string;
-          url: string;
-          img: string;
-          date: string;
-        }[];
-        carousel: { img: string };
-        details: {
-          description: string;
-          eps: string;
-          category: { id: string; name: string }[];
-        };
-      }
-    | ResponseError;
+interface IDAnimesI {
+  id: string;
+  title: string;
+  eps: {
+    id: string;
+    ep: string;
+    duration: string;
+    url: string;
+    img: string;
+  }[];
+  carousel: { img: string };
+  details: {
+    description: string;
+    eps: string;
+    category: { id: string; name: string }[];
+  };
 }
+
 export default function handleIDAnimeAPI(
   req: NextApiRequest,
-  res: NextApiResponse<IAnimeAPI | ResponseError>
+  res: NextApiResponse<IDAnimesI | ResponseError>
 ) {
   const { query } = req;
   const { id } = query;
   const animeContext = Api.find((a) => a.animes);
+  const anime = animeContext?.animes.find((a) => a.id === id);
+  if (anime) {
+    return res.status(200).json(anime);
+  }
 
-  const anime: animeI = animeContext?.animes.find((a) => a.id === id);
-
-  return anime
-    ? res.status(200).json(anime)
-    : res
-        .status(500)
-        .json({ message: `Não possível encontrar esse contexto: ${id}` });
+  return res
+    .status(500)
+    .json({ message: `Não possível encontrar esse contexto: ${id}` });
 }
--- src/pages/api/animes/index.ts
+++ src/pages/api/animes/index.ts
@@ -1,12 +1,27 @@
-import { NextApiResponse, NextApiRequest } from 'next'
-import { Api } from '../../../../data'
-import { IAnimeAPI, ResponseError } from '../../../interfaces'
-
+import { NextApiResponse, NextApiRequest } from "next";
+import { Api } from "../../../../data";
+import { IAnimeAPI, ResponseError } from "../../../interfaces";
+interface AnimesI {
+  id: string;
+  title: string;
+  eps: {
+    id: string;
+    ep: string;
+    duration: string;
+    url: string;
+    img: string;
+  }[];
+  carousel: { img: string };
+  details: {
+    description: string;
+    eps: string;
+    category: { id: string; name: string }[];
+  };
+}
 export default function handlerAnimeAPI(
   _req: NextApiRequest,
-  res: NextApiResponse<IAnimeAPI[] | undefined | ResponseError>
+  res: NextApiResponse<AnimesI[] | undefined | ResponseError>
 ) {
-  const animes = Api.find((a) => a.animes.find((a) => a))?.animes
-  return res.status(200).json(animes)
-
+  const animes = Api.find((a) => a.animes.find((a) => a))?.animes;
+  return res.status(200).json(animes);
 }
